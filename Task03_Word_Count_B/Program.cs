08a9e49c39f670c8f324b1bc31cf5e7016ac020a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-31 23:34:05 +0200

    Add files via upload

---
+++ Task03_Word_Count_B/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task03_Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsCounter = new Dictionary<string, int>();
+
+            string pathToRead = Path.Combine("data", "words.txt");
+            using (StreamReader readFile = new StreamReader(pathToRead))
+            {
+                string[] keys = readFile.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (string key in keys)
+                {
+                    wordsCounter.Add(key, 0);
+                }
+            }
+
+            pathToRead = Path.Combine("data", "text.txt");
+            using (StreamReader readFile = new StreamReader(pathToRead))
+            {
+                while (true)
+                {
+                    string nextLine = readFile.ReadLine();
+
+                    if(nextLine == null)
+                    {
+                        break;
+                    }
+
+                    string[] wordsToChek = nextLine.Split(new char[] { ',', '.', '!', '?', ' ', '-', '-' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    foreach (string word in wordsToChek)
+                    {
+                        if(wordsCounter.ContainsKey(word.ToLower()))
+                        {
+                            wordsCounter[word.ToLower()]++;
+                        }
+                    }
+                }
+            }
+
+            string pathToWrite = Path.Combine("data", "output.txt");
+            using (StreamWriter writeToFile = new StreamWriter(pathToWrite))
+            {
+                foreach (var element in wordsCounter.OrderByDescending(x => x.Value))
+                {
+                   // Console.WriteLine($"{element.Key} - {element.Value}");
+                   writeToFile.WriteLine($"{element.Key} - {element.Value}");
+                }
+            }
+            
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
